58e4c6756d59ffdcc59b9caa0861ce7af181c2de
santiagohirsch <shirsch@itba.edu.ar>
2024-04-24 12:10:49 -0300

    added CRUD methods to user_dao

--- src/persistence/user_dao.py
+++ src/persistence/user_dao.py
@@ -2,12 +2,25 @@ from src.models.user import User
 from src.schemas.user_schema import UserCreate
 from sqlalchemy.orm import Session
 
-def get_user(db: Session, user_id: int):
+def get_user(db: Session, user_id: int) -> User:
    return db.query(User).filter(User.id == user_id).first()
 
-def create_user(db: Session, user: UserCreate):
+def create_user(db: Session, user: UserCreate) -> User:
    db_user = User(email=user.email, hashed_password=f"{user.password}notreallyhashed")
    db.add(db_user)
    db.commit()
    db.refresh(db_user)
    return db_user
+
+def update_user(db: Session, user_id: int, email: str, password: str) -> User:
+   db_user = get_user(db, user_id)
+   db_user.email = email
+   db_user.hashed_password = f"{password}notreallyhashed"
+   db.commit()
+   db.refresh(db_user)
+   return db_user
+
+def delete_user(db: Session, user_id: int) -> int:
+   db.query(User).filter(User.id == user_id).delete()
+   db.commit()
+   return user_id
