9fa4d4cc1b8c39823f1e52f2bfcefc40ddf9ccc8
santiagohirsch <shirsch@itba.edu.ar>
2024-05-17 15:27:34 -0300

    feat: added db validation to all tests

--- tests/user_dao_test.py
+++ tests/user_dao_test.py
@@ -1,8 +1,15 @@
 import pytest
 from sqlalchemy import text
 from src.schemas.user_schema import UserCreate
-from src.persistence.user_dao import create_user, get_user, update_user, delete_user
+from src.persistence.user_dao import create_user, get_user, update_user, delete_user, count_users, count_users_where_email
 from tests.test_setup import setup_database, setup_function
+
+# DEFAULT DATABASE SETUP
+# INSERT INTO user (id, email, hashed_password)
+# VALUES
+#     (1, 'user1@example.com', 'examplenotreallyhashed1'),
+#     (2, 'user2@example.com', 'examplenotreallyhashed2'),
+#     (3, 'user3@example.com', 'examplenotreallyhashed3');
 
 
 @pytest.mark.parametrize(
@@ -18,6 +25,8 @@ def test_create_user(setup_database, email, password, setup_function):
    user = create_user(setup_database, user_create)
    assert user.email == email
    assert user.hashed_password == f"{password}notreallyhashed"
+   assert count_users(setup_database) == 4
+   assert count_users_where_email(setup_database, email) == 1
 
 def test_get_user(setup_database, setup_function):
    print("\nTesting getting user")
@@ -25,14 +34,23 @@ def test_get_user(setup_database, setup_function):
    assert user.id == 1
    assert user.email == "user1@example.com"
    assert user.hashed_password == "examplenotreallyhashed1"
+   assert count_users(setup_database) == 3
+   assert count_users_where_email(setup_database, "user1@example.com") == 1
 
 def test_update_user(setup_database, setup_function): 
    print("\nTesting updating user")
-   updated_user = update_user(setup_database, user_id=1, email="newemail@example.com", password="newpassword")
-   assert updated_user.email == "newemail@example.com"
-   assert updated_user.hashed_password == "newpasswordnotreallyhashed"
+   new_email = "newemail@example.com"
+   new_password = "newpassword"
+   updated_user = update_user(setup_database, user_id=1, email=new_email, password=new_password)
+   assert updated_user.email == new_email
+   assert updated_user.hashed_password == new_password + "notreallyhashed"
+   assert count_users(setup_database) == 3
+   assert count_users_where_email(setup_database, new_email) == 1
+
 
 def test_delete_user(setup_database, setup_function):
    print("\nTesting deleting user")
    deleted_user_id = delete_user(setup_database, user_id=1)
    assert deleted_user_id == 1
+   assert count_users(setup_database) == 2
+   assert count_users_where_email(setup_database, "user1@example.com") == 0
